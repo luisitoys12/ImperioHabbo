d4861ae1562bfc201327f4e12fe5a692b3fd5b23
Mario Vega <marioalbe123@gmail.com>
2025-04-02 00:41:46 -0500

    deploy1

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,3 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Prueba con '/' si sigue fallando
-  build: {
-    outDir: 'dist', // Confirma que la carpeta de salida es correcta
-  },
-});
