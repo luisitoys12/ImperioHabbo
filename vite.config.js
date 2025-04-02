6d115a408987af3b41bcd3152a983fa4d97be6b4
Mario Vega <marioalbe123@gmail.com>
2025-04-02 00:18:13 -0500

    despues del build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173, // Asegúrate de que es el puerto correcto
+  },
+  preview: {
+    port: 4173, // Vercel usa este puerto por defecto para previsualización
+  }
+});
