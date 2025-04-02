25bf1ad9f437f5269935d7321cb8ab3d033c1759
Mario Vega <marioalbe123@gmail.com>
2025-04-02 00:24:15 -0500

    despues del build3

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
-    host: true,
-    strictPort: true,
-    port: 5173, // Asegúrate de que es el puerto correcto
-  },
-  preview: {
-    port: 4173, // Vercel usa este puerto por defecto para previsualización
+    allowedHosts: ["imperiohabbo.onrender.com", "imperio-habbo.vercel.app"]
   }
-});
+};
