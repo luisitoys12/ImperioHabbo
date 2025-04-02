58125d147e126e2e5f105cb02277831daaa1947b
Mario Vega <marioalbe123@gmail.com>
2025-04-02 00:43:20 -0500

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Asegura rutas correctas en producción
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    allowedHosts: ["imperiohabbo.onrender.com", "imperio-habbo.vercel.app"], // Permite estos hosts
+  },
+});
